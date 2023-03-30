fab035fbb1261df525e45dc7a87ecab56047bf77
zachbaker55 <thezachbaker55@gmail.com>
2023-03-30 14:26:56 -0400

    MusicLabel added
    the music label added, will integrate with the music player once that is created

---
+++ Assets/Scripts/MusicLabel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MusicLabel : MonoBehaviour
+{
+    private TMP_Text _text;
+    public TMP_Text Text 
+    { 
+        get { return _text; }
+    }
+
+
+    [Range(0.0f,3.0f)]
+    [SerializeField] private float labelTime = 1.0f;
+    private string _musicName = "Example Song Name";
+    public string MusicName 
+    { 
+        get { return _musicName; }
+    }
+
+    private void Awake() {
+        _text = GetComponent<TMP_Text>();
+    }
+
+    //TODO: Move from start to a music event
+    private void Start() 
+    {
+        ActivateLabel(_musicName);
+    }
+
+    private void ActivateLabel(string musicName) 
+    {
+        _musicName = musicName;
+        StartCoroutine(Label());
+    }
+
+    IEnumerator Label() 
+    {
+        foreach (char c in _musicName) {
+            _text.text += c;
+            yield return new WaitForSeconds(labelTime * 0.1f);
+        }
+    }
+
+}
