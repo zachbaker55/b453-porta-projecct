5cba4dcc02821218641c7d74f70728ba4b6114ce
zachbaker55 <thezachbaker55@gmail.com>
2023-03-30 15:07:27 -0400

    Added some Game UI elements

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private Sprite full;
+    [SerializeField] private Sprite threeFourth;
+    [SerializeField] private Sprite half;
+    [SerializeField] private Sprite oneFourth;
+
+    //TODO: Make it work
+}
