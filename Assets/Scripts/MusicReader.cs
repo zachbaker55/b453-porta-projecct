374ae0e747afa04914d85ab8be9cca8419a797ae
zachbaker55 <thezachbaker55@gmail.com>
2023-04-13 17:24:34 -0400

    Another quick fix

--- Assets/Scripts/MusicReader.cs
+++ Assets/Scripts/MusicReader.cs
@@ -91,6 +91,5 @@ public class MusicReader : MonoBehaviour
             //If json has an incorrect note- stop and do nothing
             return;
         }
-        Debug.Log(musicNote.note);
     }
 }
